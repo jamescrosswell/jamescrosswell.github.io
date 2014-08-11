dd5b0ddd7978951b6a8adf5c41a1a0049af08d5b
James Crosswell <james.crosswell@gmail.com>
2014-08-12 00:15:54 +0200

    - Replaced rainbow highlighting with highlight.js - Added docs on strings and arrays

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,6 @@
 var app = angular.module('tut',[]);
 
 app.controller('IndexCtrl', function($scope){
-
 });
 
 app.directive('codeBlock', function(){
@@ -16,12 +15,14 @@ app.directive('codeBlock', function(){
          transclude:true,
          template: '<div ng-transclude></div>',
          link:function(scope, element){
-             var innerText = element.text();
              // If the first character is a new line then remove this
+             var innerText = element.text();
              innerText = innerText.replace(/^(\n)/g, '');
-             // Now get the highlighted code
-             Rainbow.color(innerText, scope.lang, function(highlighted_code) {
-                 element.html('<pre><code>' + highlighted_code + '</code></pre>');
+             element.text(innerText);
+
+             // Now highlight the code
+             element.each(function(i, block) {
+                 hljs.highlightBlock(block);
              });
          }
      };
