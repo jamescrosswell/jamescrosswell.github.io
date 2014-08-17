b30cc950ce621d6542f2352691f88f8fc159722b
James Crosswell <james.crosswell@gmail.com>
2014-08-17 21:12:17 +0200

    Added docs on hashes and symbols

--- public/js/app.js
+++ public/js/app.js
@@ -18,7 +18,25 @@ app.directive('codeBlock', function(){
              // If the first character is a new line then remove this
              var innerText = element.text();
              innerText = innerText.replace(/^(\n)/g, '');
-             element.text(innerText);
+
+             // split the content of the PRE element into an array of lines
+             var lines = innerText.split( '\n' );
+
+             // the last line is expected to be an empty line - remove it
+             if ( lines.length > 1 && lines[ lines.length - 1 ].trim() === '' ) {
+                 lines.pop();
+             }
+
+             // how much white-space do we need to remove form each line?
+             var offset = lines[ 0 ].match( /^\s*/ )[ 0 ].length;
+
+             // remove the exess white-space from the beginning of each line
+             lines = lines.map( function ( line ) {
+                 return line.slice( offset );
+             });
+
+             // set this new content for the code-block element
+             element.text( lines.join( '\n' ) );
 
              // Now highlight the code
              element.each(function(i, block) {
